271afd616bb42ea564660d1466d6a7dcb2535d5e
Isabel Maskrey <isabel@satyrn.dev>
2022-05-21 15:11:12 -0400

    🔨 Add CI build scripts Make build.ps1 platform-agnostic Add build-with-powershell.yml

--- build.ps1
+++ build.ps1
@@ -1,22 +1,22 @@
-$buildDir = "$PSScriptRoot\.build"
+$buildDir = "./.build"
 
 if (-not (Test-Path $buildDir)) {
   New-Item -ItemType Directory $buildDir
 }
 
-$packFile = "$PSScriptRoot\pack.mcmeta"
+$packFile = "./pack.mcmeta"
 
 if (Test-Path $packFile) {
   # Load pack json to get file name
   $pack = (Get-Content $packFile | Out-String | ConvertFrom-Json)
   if ($pack.meta.baseArchiveName -and $pack.meta.version) {
-    $outFile = "$buildDir\$($pack.meta.baseArchiveName)-$($pack.meta.version).zip"
+    $outFile = "$buildDir/$($pack.meta.baseArchiveName)-$($pack.meta.version).zip"
 
     if (Test-Path $outFile) {
       Remove-Item $outFile -force
       Write-Output "Removed previous build at $outFile"
     }
 
-    &"C:\Program Files\7-zip\7z.exe" a -tzip -i"!$PSScriptRoot\*" -x"!$PSScriptRoot\.*" $outFile
+    7z a -tzip -i"!$PSScriptRoot/*" -x"!$PSScriptRoot/.*" $outFile
   }
 }
